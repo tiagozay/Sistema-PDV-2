9e676f01600c5874097448c8387535b74ac318ff
Tiago zay <tiagozay@gmail.com>
2022-09-23 11:20:56 -0300

    Implementando SimpleMaskMoney
    Signed-off-by: Tiago zay <tiagozay@gmail.com>

--- JavaScript/mascaraDeDinheiro.js
+++ JavaScript/mascaraDeDinheiro.js
@@ -1,54 +1,16 @@
-jQuery(function() {   
-    jQuery("#form-vender-produto-avulso__div-input-preco__input").maskMoney({
-    prefix:'R$ ', 
-    thousands:'.', 
-    decimal:','
-})
+const args = {
+    allowNegative: false,
+    negativeSignAfter: false,
+    prefix: 'R$ ',
+    fixed: true,
+    fractionDigits: 2,
+    decimalSeparator: ',',
+    thousandsSeparator: '.',
+    cursor: 'move'
+};
 
-});
-
-jQuery(function() {   
-    jQuery("#input-desconto-da-venda__valor").maskMoney({
-    prefix:'R$ ', 
-    thousands:'.', 
-    decimal:','
-})
-
-});
-
-jQuery(function() {   
-    jQuery("#input-valor-pago-da-venda__valor").maskMoney({
-    prefix:'R$ ', 
-    thousands:'.', 
-    decimal:','
-})
-
-});
-
-
-jQuery(function() {   
-    jQuery("#dialog-modal-cadastrar-produto__form__divCampo__input-vlUnitario").maskMoney({
-    prefix:'R$ ', 
-    thousands:'.', 
-    decimal:','
-})
-
-});
-
-jQuery(function() {   
-    jQuery("#dialog-modal-editar-produto__form__divCampo__input-vlUnitario").maskMoney({
-    prefix:'R$ ', 
-    thousands:'.', 
-    decimal:','
-})
-
-});
-
-// jQuery(function() {   
-//     jQuery("#modal-informar-quantidade-de-produto__input-quantidade").maskMoney({
-//     prefix:'R$ ', 
-//     thousands:'.', 
-//     decimal:','
-// })
-
-// });
+SimpleMaskMoney.setMask('#form-vender-produto-avulso__div-input-preco__input', args);
+SimpleMaskMoney.setMask('#input-desconto-da-venda__valor', args);
+SimpleMaskMoney.setMask('#input-valor-pago-da-venda__valor', args);
+SimpleMaskMoney.setMask('#dialog-modal-cadastrar-produto__form__divCampo__input-vlUnitario', args);
+SimpleMaskMoney.setMask('#dialog-modal-editar-produto__form__divCampo__input-vlUnitario', args);
