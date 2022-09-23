7f4d433b40204ff759a2e7128c3c4605eeea4acc
Tiago zay <tiagozay@gmail.com>
2022-09-23 10:05:05 -0300

    Fazendo com que as credenciais de conexão de banco venham de um arquivo
    Signed-off-by: Tiago zay <tiagozay@gmail.com>

--- back-end/src/Infraestrutura/Persistencia/ConnectionCreator.php
+++ back-end/src/Infraestrutura/Persistencia/ConnectionCreator.php
@@ -1,15 +1,18 @@
 <?php
-    require 'credenciais-banco.php';
-
     namespace PDV\Infraestrutura\Persistencia;
+
+    require 'credenciais-banco.php';
 
     class ConnectionCreator
     {
         public static function CreateConnection(): \PDO
         {
-            return new \PDO("mysql:host=$host;dbname=$db_name", "$user", "$password");
+            return new \PDO(
+                "mysql:host=".HOST.";dbname=".DB_NAME,
+                USER,
+                PASSWORD
+            );
         }
     }
-
 
 ?>
