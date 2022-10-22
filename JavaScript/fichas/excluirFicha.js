ab09f1cafb1f782c23f4c0805d155a59a181bed2
Tiago zay <tiagozay@gmail.com>
2022-10-22 11:23:44 -0300

    Implementando ORM e fecth para excluír ficha
    Signed-off-by: Tiago zay <tiagozay@gmail.com>

--- JavaScript/fichas/excluirFicha.js
+++ JavaScript/fichas/excluirFicha.js
@@ -1,6 +1,5 @@
 function excluir_ficha(event)
 {
-
     let id_ficha = event.target.dataset.id;
 
     let confirmacao = confirm("Excluir ficha permanentemente?");
@@ -8,33 +7,31 @@ function excluir_ficha(event)
 
     zayDataTable__fichas.ativa_loader_de_um_registro(id_ficha);
 
-    zay_request(
-        'POST',
+    fetch(
         './back-end/excluirFicha.php',
-        {id: id_ficha},
-        ( resposta ) => {
-
-            zayDataTable__fichas.desativa_loader_de_um_registro(id_ficha);
-
-            try{
-                resposta = JSON.parse(resposta);
-            }catch{
-                abrir_mensagem_lateral_da_tela("Não foi possível excluir ficha!");
-                return;
-            }
-
-            if(resposta.sucesso){
-                abrir_mensagem_lateral_da_tela("Ficha excluída com sucesso!");
-                zayDataTable__fichas.remove_registro(id_ficha);
-            }
-
-
-        },
-        () => {
-            zayDataTable__fichas.desativa_loader_de_um_registro(id_ficha);
-            abrir_mensagem_lateral_da_tela("Não foi possível excluir ficha!");
-      
+        {   
+            headers: {'Content-Type': 'application/x-www-form-urlencoded'},
+            method: 'POST',
+            body: `id=${id_ficha}`
         }
     )
+    .then( resposta => {
+        
+        zayDataTable__fichas.desativa_loader_de_um_registro(id_ficha);
 
+        if(resposta.ok){
+            abrir_mensagem_lateral_da_tela("Ficha excluída com sucesso!");
+            zayDataTable__fichas.remove_registro(id_ficha);
+        }else{
+            abrir_mensagem_lateral_da_tela("Não foi possível excluir ficha!")
+        }
+
+
+    } )
+    .catch( () => {
+
+        zayDataTable__fichas.desativa_loader_de_um_registro(id_ficha);
+        abrir_mensagem_lateral_da_tela("Não foi possível excluir ficha!");
+
+    } );
 }
